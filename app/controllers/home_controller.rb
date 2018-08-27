b6cbf47cdd31a07bacef32336acecfeeb2ba3ad1
ruritoblogger <rurito.blog@gmail.com>
2018-08-27 17:44:28 +0900

    add new menber

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,9 @@
 class HomeController < ApplicationController
+
   def top
   end
+
+  def new
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   get "/home/top" => "home#top"
+  get "/home/new" => "home#new"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
