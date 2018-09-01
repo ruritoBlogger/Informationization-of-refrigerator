6db7a431f6e0f7779537f11676be655c995eec06
ruritoblogger <rurito.blog@gmail.com>
2018-09-01 23:38:15 +0900

    add image_name to User model

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_01_124503) do
+ActiveRecord::Schema.define(version: 2018_09_01_143613) do
 
   create_table "users", force: :cascade do |t|
     t.string "user_name"
     t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_name"
   end
 
 end
