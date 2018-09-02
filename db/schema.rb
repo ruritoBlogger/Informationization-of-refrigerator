29b50ca6047c3d0bbec3956f06d931c1b4b4e012
ruritoblogger <rurito.blog@gmail.com>
2018-09-02 19:09:22 +0900

    add introduction to User model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_01_143613) do
+ActiveRecord::Schema.define(version: 2018_09_02_100803) do
 
   create_table "users", force: :cascade do |t|
     t.string "user_name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2018_09_01_143613) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "image_name"
+    t.text "introduction"
   end
 
 end
