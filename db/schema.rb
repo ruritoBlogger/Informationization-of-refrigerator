461970fca559c3cc133ec6de49f4c36b1ca166ce
ruritoblogger <rurito.blog@gmail.com>
2018-09-04 00:34:18 +0900

    create and delete test model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_03_130933) do
+ActiveRecord::Schema.define(version: 2018_09_03_153331) do
 
   create_table "food1s", force: :cascade do |t|
     t.integer "user_id"
@@ -23,6 +23,11 @@ ActiveRecord::Schema.define(version: 2018_09_03_130933) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
+  end
+
+  create_table "food2s", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
