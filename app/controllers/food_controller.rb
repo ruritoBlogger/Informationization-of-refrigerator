4afe88c4dee536812141248950ed626ab03186b3
ruritoblogger <rurito.blog@gmail.com>
2018-09-10 09:17:07 +0900

    divide flash and redirect about can make new food or not

--- app/controllers/food_controller.rb
+++ app/controllers/food_controller.rb
@@ -16,7 +16,7 @@ class FoodController < ApplicationController
                            limittype: true)
 
     @Fleshfoods = Food.where(user_id: session[:user_id],
-                              limittype: false)
+                             limittype: false)
 
     @modes = Mode.where(user_id: session[:user_id])
   end
@@ -50,67 +50,71 @@ class FoodController < ApplicationController
                      amount: params[:amount],
                      name: params[:name],
                      yetamount: 100)
-    @food.save
-    @food = Food.find_by(user_id: session[:user_id],
-                        limitYear: params[:limitYear],
-                        limitMonth: params[:limitMonth],
-                        limitDay: params[:limitDay],
-                        amount: params[:amount],
-                        name: params[:name],
-                        yetamount: 100)
-
-    #受け取ったモードを保存する
-    #@modes = Mode.where(user_id: session[:user_id])
-    #@modes.each do |mode|
-    #  if params[:addmode]
-    #
-    #  end
-    #end
-
-    #食べ物の画像の保存
-    if params[:image_name]
-      @food.image_name = "#{@food.id}.jpg"
-      image = params[:image_name]
-      File.binwrite("public/food#{session[:user_id]}_images/#{@food.image_name}",image.read)
+    if !(@food.save)
+      flash[:notice_fail] = "登録に失敗しました"
+      redirect_to("/food/new")
     else
-      #ネットからのurlで画像を用意する
-      @food.image_name = "https://uds.gnst.jp/rest/img/1ew287ve0000/s_006z.jpg?t=1506082927"
+      @food = Food.find_by(user_id: session[:user_id],
+                           limitYear: params[:limitYear],
+                           limitMonth: params[:limitMonth],
+                           limitDay: params[:limitDay],
+                           amount: params[:amount],
+                           name: params[:name],
+                           yetamount: 100)
+
+      #受け取ったモードを保存する
+      #@modes = Mode.where(user_id: session[:user_id])
+      #@modes.each do |mode|
+      #  if params[:addmode]
+      #
+      #  end
+      #end
+
+      #食べ物の画像の保存
+      if params[:image_name]
+        @food.image_name = "#{@food.id}.jpg"
+        image = params[:image_name]
+        File.binwrite("public/food#{session[:user_id]}_images/#{@food.image_name}", image.read)
+      else
+        #ネットからのurlで画像を用意する
+        @food.image_name = "https://uds.gnst.jp/rest/img/1ew287ve0000/s_006z.jpg?t=1506082927"
+      end
+
+      #  @limitYear = params[:limitYear]
+      #  @limitMonth = params[:limitMonth]
+      #  @limitDay = params[:limitDay]
+      #  @amount = params[:amount]
+      #  @name = params[:name]
+
+      #賞味期限か消費期限かの判断
+      if params[:limit]
+        @food.limittype = true
+      else
+        @food.limittype = false
+      end
+
+      #加工食品か生鮮食品かの判断
+      if params[:foodtype]
+        @food.foodtype = true
+      else
+        @food.foodtype = false
+      end
+
+      #個数の単位の判断
+      if params[:amounttype] == 0
+        @food.amounttype = "g"
+      elsif params[:amounttype] == 1
+        @food.amounttype = "mL"
+      elsif params[:amounttype] == 2
+        @food.amounttype = "人前"
+      else
+        @food.amounttype = "個・本"
+      end
+
+      @food.save
+      flash[:notice] = "食べ物の新規登録を行いました"
+      redirect_to("/food/index")
     end
-
-    #  @limitYear = params[:limitYear]
-    #  @limitMonth = params[:limitMonth]
-    #  @limitDay = params[:limitDay]
-    #  @amount = params[:amount]
-    #  @name = params[:name]
-
-    #賞味期限か消費期限かの判断
-    if params[:limit]
-      @food.limittype = true
-    else
-      @food.limittype = false
-    end
-
-    #加工食品か生鮮食品かの判断
-    if params[:foodtype]
-      @food.foodtype = true
-    else
-      @food.foodtype = false
-    end
-
-    #個数の単位の判断
-    if params[:amounttype] == 0
-      @food.amounttype = "g"
-    elsif params[:amounttype] == 1
-      @food.amounttype = "mL"
-    elsif params[:amounttype] == 2
-      @food.amounttype = "人前"
-    else
-      @food.amounttype = "個・本"
-    end
-
-    @food.save
-    flash[:notice] = "食べ物の新規登録を行いました"
-    redirect_to("/food/index")
   end
 
   #食べ物の量の更新
