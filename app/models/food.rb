3c1d5152c0c59a67f63f4749fa5eaf5cef992e42
ruritoblogger <rurito.blog@gmail.com>
2018-09-10 09:07:19 +0900

    add validates to Food model

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,2 +1,15 @@
 class Food < ApplicationRecord
+
+  validates :user_id, {presence: true}
+  validates :limittype, {presence: true}
+  validates :foodtype, {presence: true}
+  validates :name, {presence: true}
+  validates :amount, {presence: true}
+  validates :yetamount, {presence: true}
+  validates :image_name, {presence: true}
+  validates :amounttype, {presence: true}
+  validates :limitYear, {presence: true}
+  validates :limitMonth, {presence: true}
+  validates :limitDay, {presence: true}
+
 end
