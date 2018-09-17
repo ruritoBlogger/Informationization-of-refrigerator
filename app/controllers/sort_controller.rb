ea73388b41d5da1d0c617bc9b572e63aae140ce0
ruritoblogger <rurito.blog@gmail.com>
2018-09-17 20:50:02 +0900

    add info to sort main page

--- app/controllers/sort_controller.rb
+++ app/controllers/sort_controller.rb
@@ -1,2 +1,8 @@
 class SortController < ApplicationController
+
+  def main
+    @modes = Mode.where(user_id: session[:user_id])
+    @foods = Food.where(user_id: session[:user_id]).order(:limitday)
+  end
+
 end
